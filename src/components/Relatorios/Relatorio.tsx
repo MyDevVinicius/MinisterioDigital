f63adff2464028da1dda178e0882a65873d476f2
Vinicius Nascimento <nascimentovrn@outlook.com>
2025-01-05 17:09:36 -0400

    update pdf

--- src/components/Relatorios/Relatorio.tsx
+++ src/components/Relatorios/Relatorio.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 interface FilterComponentProps {
   onFilterChange: (filters: {
@@ -23,11 +25,12 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
   const [usuarios, setUsuarios] = useState<{ id: string; nome: string }[]>([]);
   const [dados, setDados] = useState<
     {
+      observacao: string;
       tipo: string;
-      subtipo: string;
+      valor: string;
       formaPagamento: string;
       data: string;
-      usuario: string;
+      usuarioNome: string;
     }[]
   >([]);
   const [loading, setLoading] = useState<boolean>(false);
@@ -98,6 +101,128 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
     const month = String(dateObj.getMonth() + 1).padStart(2, "0");
     const year = dateObj.getFullYear();
     return `${day}/${month}/${year}`;
+  };
+
+  // Função para exportar os dados para um PDF
+  const handleExportarPDF = async () => {
+    const doc = new jsPDF();
+    const titulo = "Relatório";
+    const dataAtual = new Date().toLocaleDateString();
+    const horaAtual = new Date().toLocaleTimeString();
+
+    // Obter informações do usuário
+    const email = localStorage.getItem("email");
+    const nomeBanco = localStorage.getItem("nome_banco");
+
+    if (!email || !nomeBanco) {
+      console.log("Email ou nome do banco não encontrado no localStorage.");
+      return;
+    }
+
+    let usuario = null;
+    try {
+      const response = await fetch(
+        `/api/usuarios?email=${email}&nome_banco=${nomeBanco}`,
+      );
+      if (!response.ok) throw new Error("Erro ao buscar dados do usuário.");
+      usuario = await response.json();
+    } catch (error) {
+      console.error(error.message);
+      console.log("Erro ao buscar informações do usuário.");
+      return;
+    }
+
+    if (!usuario) {
+      console.log("Usuário não encontrado.");
+      return;
+    }
+
+    // Configurações de fonte
+    const fontePadrao = "helvetica";
+    const tamanhoFonteTitulo = 14;
+    const tamanhoFonteTexto = 10;
+    const margemEsquerda = 14;
+
+    // Carregar a logo como base64
+    const logoBase64 = await fetch("/logosoft.png")
+      .then((res) => res.blob())
+      .then((blob) => {
+        return new Promise<string>((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onloadend = () => resolve(reader.result as string);
+          reader.onerror = () => reject("Erro ao carregar imagem.");
+          reader.readAsDataURL(blob);
+        });
+      });
+
+    // Adicionar logo
+    const logoWidth = 40; // Largura da logo
+    const logoHeight = 18; // Altura da logo
+    const logoX = margemEsquerda; // Posição X
+    const logoY = 10; // Posição Y
+    doc.addImage(logoBase64, "PNG", logoX, logoY, logoWidth, logoHeight);
+
+    // Adicionar data, hora e informações do usuário
+    doc.setFont(fontePadrao, "normal");
+    doc.setFontSize(tamanhoFonteTexto);
+    const textoDataHora = `Data: ${dataAtual}  |  Hora: ${horaAtual}`;
+    const margemDireita = doc.internal.pageSize.width - margemEsquerda;
+    doc.text(textoDataHora, margemDireita, 20, { align: "right" });
+
+    const textoUsuario = `${usuario.nome} - ${usuario.cargo}`;
+    doc.text(textoUsuario, margemDireita, 25, { align: "right" });
+
+    // Adicionar título
+    doc.setFontSize(tamanhoFonteTitulo);
+    doc.setFont(fontePadrao, "bold");
+    doc.text(titulo, margemEsquerda, logoY + logoHeight + 10);
+
+    // Configuração da tabela
+    const tableColumn = [
+      "Observação",
+      "Tipo",
+      "Valor",
+      "Forma de Pagamento",
+      "Data de Lançamento",
+      "Lançamento Feito Por",
+    ];
+    const tableRows: string[][] = [];
+
+    dados.forEach((item) => {
+      const row = [
+        item.observacao,
+        item.tipo,
+        item.valor,
+        item.formaPagamento,
+        formatarData(item.data),
+        item.usuarioNome,
+      ];
+      tableRows.push(row);
+    });
+
+    // Adicionar tabela
+    doc.autoTable({
+      head: [tableColumn],
+      body: tableRows,
+      startY: logoY + logoHeight + 20,
+      styles: {
+        halign: "center", // Centralizar texto nas células
+        valign: "middle", // Centralizar verticalmente
+        fontSize: tamanhoFonteTexto,
+      },
+      columnStyles: {
+        0: { cellWidth: "auto" },
+        1: { cellWidth: "auto" },
+        2: { cellWidth: "auto" },
+        3: { cellWidth: "auto" },
+        4: { cellWidth: "auto" },
+        5: { cellWidth: "auto" },
+      },
+      tableWidth: "wrap", // Ajusta a tabela para ocupar a largura da página
+    });
+
+    // Salvar o arquivo PDF
+    doc.save("Relatorio.pdf");
   };
 
   return (
@@ -237,72 +362,71 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
             ))}
           </select>
         </div>
+      </div>
 
+      <div className="mt-6 flex justify-end space-x-4">
         <button
           onClick={handleGerarRelatorio}
-          className="mt-4 w-full rounded-md bg-blue-500 py-2 text-white"
+          className="rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
         >
-          Gerar Relatório
+          {loading ? "Carregando..." : "Gerar Relatório"}
+        </button>
+        <button
+          onClick={handleExportarPDF}
+          className="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+        >
+          Exportar PDF
         </button>
       </div>
 
-      <div className="mt-8">
-        <h3 className="text-md mb-4 font-semibold text-gray-700">Resultados</h3>
-        <table className="w-full border-collapse border border-gray-300">
-          <thead>
-            <tr className="bg-gray-200">
-              <th className="border border-gray-300 px-4 py-2">Tipo</th>
-              <th className="border border-gray-300 px-4 py-2">
-                Observação
-              </th>{" "}
-              <th className="border border-gray-300 px-4 py-2">Subtipo</th>
-              {/* Nova coluna */}
-              <th className="border border-gray-300 px-4 py-2">
-                Forma de Pagamento
-              </th>
-              <th className="border border-gray-300 px-4 py-2">Data</th>
-              <th className="border border-gray-300 px-4 py-2">Usuário</th>{" "}
-              {/* Usuário já vai mostrar o nome */}
-            </tr>
-          </thead>
-          <tbody>
-            {dados.length > 0 ? (
-              dados.map((dado, index) => (
+      <div className="mt-6">
+        <h3 className="mb-4 text-lg font-semibold text-gray-700">Resultados</h3>
+        {dados.length === 0 ? (
+          <p>Nenhum dado encontrado.</p>
+        ) : (
+          <table className="w-full table-auto border-collapse border border-gray-300">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 px-4 py-2">Observação</th>
+                <th className="border border-gray-300 px-4 py-2">Tipo</th>
+                <th className="border border-gray-300 px-4 py-2">Valor</th>
+                <th className="border border-gray-300 px-4 py-2">
+                  Forma de Pagamento
+                </th>
+                <th className="border border-gray-300 px-4 py-2">
+                  Data de Lançamento
+                </th>
+                <th className="border border-gray-300 px-4 py-2">
+                  Lançado por
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {dados.map((item, index) => (
                 <tr key={index}>
                   <td className="border border-gray-300 px-4 py-2">
-                    {dado.tipo}
+                    {item.observacao}
                   </td>
                   <td className="border border-gray-300 px-4 py-2">
-                    {dado.observacao}
-                  </td>{" "}
-                  {/* Exibe a observação */}
-                  <td className="border border-gray-300 px-4 py-2">
-                    {dado.subtipo}
+                    {item.tipo}
                   </td>
                   <td className="border border-gray-300 px-4 py-2">
-                    {dado.formaPagamento}
+                    {item.valor}
                   </td>
                   <td className="border border-gray-300 px-4 py-2">
-                    {formatarData(dado.data)}
+                    {item.formaPagamento}
                   </td>
                   <td className="border border-gray-300 px-4 py-2">
-                    {dado.usuarioNome}
-                  </td>{" "}
-                  {/* Exibe o nome do usuário */}
+                    {formatarData(item.data)}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    {item.usuarioNome}
+                  </td>
                 </tr>
-              ))
-            ) : (
-              <tr>
-                <td
-                  colSpan={6}
-                  className="border border-gray-300 px-4 py-2 text-center"
-                >
-                  Nenhum dado encontrado
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
